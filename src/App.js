e1ae628e50edda7c2726e41187ae47c17a6888e1
Kostya <koortttt@gmail.com>
2019-01-12 12:21:20 +0200

    some actions with text, button, text in button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        showGreetings: false,
+    };
+
+    showMsg = () => {
+        this.setState({
+            showGreetings: !this.state.showGreetings,
+        })
+    };
+
+    render() {
+        let greetings = null;
+        let btnValue = 'Open';
+        if (this.state.showGreetings) {
+            greetings = <p>Hello world dude!</p>;
+            btnValue = 'Close'
+        }
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    {greetings}
+                    <button onClick={this.showMsg}>{btnValue}</button>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
