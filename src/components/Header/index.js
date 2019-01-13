c8e595f47be7353e27777dc5dd705f9ce4aa6a3f
Kostya <koortttt@gmail.com>
2019-01-13 21:14:44 +0200

    adding desing                  added fontawesоme

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,6 +22,7 @@ class Header extends Component {
                 </div>
                 <div className="control">
                     <UserControl/>
+
                 </div>
             </header>
         )
